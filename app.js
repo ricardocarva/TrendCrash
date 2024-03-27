5e7df270a9760818e413a0a0120f31a359c0388b
thomas-martin-uf <thomas.martin@ufl.edu>
2024-03-27 19:03:51 -0400

    made a small ui to help with data querying from the front end, won't be the final ui

--- app.js
+++ app.js
@@ -1,26 +1,60 @@
 import { setupOracleConnection } from "./config/dbs.js";
 import express from "express";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import path from "path";
+
 const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 
 // load config file
 dotenv.config({ path: "./config/config.env" });
+
+// middleware to serve static files in public
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded({ extended: true }));
 
 // try to setup oracle connection
 let connection = await setupOracleConnection();
 
 // endpoint to fetch data from Oracle database, data base connection made on each request
 // may want to rework this to just leave the connection open so that we don't have overhead on the request
-app.get("/data", async (req, res) => {
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.post("/data", async (req, res) => {
     // raw query
     // const query = `SELECT * FROM RCARVALHEIRA.ACCIDENTMASTER ORDER BY id FETCH FIRST 10000 ROWS ONLY;`;
-    const query = `SELECT * FROM RCARVALHEIRA.ACCIDENTMASTER WHERE ROWNUM <= 10000`;
+    // console.log(req.body.data);
+
+    // todo need to confirm valid sql here, we don't need to but could i guess
+
+    const selectFrom = "SELECT DISTINCT * FROM";
+    const tableName = "RCARVALHEIRA.STATES";
+    const whereClause = "WHERE ROWNUM <= 1000";
+    const cityQuery = "select * from city where rownum <= 10000";
+    // const query = `${selectFrom} ${tableName} ${whereClause}`;
+
+    // strip off semicolon
+    const query = req.body.data.replace(/;/g, "");
+    console.log(query);
 
     try {
+        const start = Date.now();
         const result = await connection.execute(query);
+        const end = Date.now();
+        const numTuples = result.rows.length;
         const resObj = {
             query,
             result: result.rows,
+            execution_time: `${numTuples} tuples returned in ${end - start}ms`,
         };
 
         console.log("\nresult found", resObj);
