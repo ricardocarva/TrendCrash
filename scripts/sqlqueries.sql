16eaa47e6328706773be18331b060deb29dd72a4
jess-win <jrizzo.br@gmail.com>
2024-04-08 17:02:58 -0400

    added query 1

--- scripts/sqlqueries.sql
+++ scripts/sqlqueries.sql
@@ -1,6 +1,46 @@
 /* Query 1: Relationship between accidents and traffic details */
+SELECT 
+    s.name AS state,
+    l.city,
+    a.accident_date,
+    ROUND(AVG(a.severity), 2) AS severity_avg,
+    COUNT(*) AS accident_count,
+    SUM(
+        CASE WHEN td.bump = 1 THEN 1 ELSE 0 END +
+        CASE WHEN td.crossing = 1 THEN 1 ELSE 0 END +
+        CASE WHEN td.give_way = 1 THEN 1 ELSE 0 END +
+        CASE WHEN td.junction = 1 THEN 1 ELSE 0 END +
+        CASE WHEN td.no_exit = 1 THEN 1 ELSE 0 END +
+        CASE WHEN td.railway = 1 THEN 1 ELSE 0 END +
+        CASE WHEN td.railway_station = 1 THEN 1 ELSE 0 END +
+        CASE WHEN td.roundabout = 1 THEN 1 ELSE 0 END +
+        CASE WHEN td.stop = 1 THEN 1 ELSE 0 END +
+        CASE WHEN td.traffic_calming = 1 THEN 1 ELSE 0 END +
+        CASE WHEN td.traffic_signal = 1 THEN 1 ELSE 0 END +
+        CASE WHEN td.turning_loop = 1 THEN 1 ELSE 0 END
+    ) AS available_traffic_details_count
+FROM 
+    "THOMAS.MARTIN".Accident a
+    JOIN
+    RCARVALHEIRA.TrafficDetails td ON td.traffic_details_id = a.traffic_details_id
+    JOIN 
+    RCARVALHEIRA.LocationDetails l ON l.location_id = a.location_id
+    JOIN
+    MSTRENGES.STATE s ON s.state_id = l.state_id
+WHERE
+    l.city IS NOT NULL 
+GROUP BY 
+    s.name,
+    l.city,
+    a.accident_date
+ORDER BY 
+    s.name ASC,
+    l.city ASC,
+    a.accident_date ASC
+FETCH FIRST 50 ROWS ONLY;
 
 /* Query 2: Relationship between Covid-19 lockdowns and traffic accidents */
+
 
 /* Query 3: Relationship between unemployment rates and accident rates */
 SELECT 
@@ -55,10 +95,9 @@ ORDER BY
 	c.year,
 	c.quarter;
 
-
 /* Query 5: Correlation between road conditions and accident rates */
 SELECT
-    s.name,
+    s.name AS state,
     rc.year,
     rc.percent_acceptable_miles,
     COUNT(a.accident_id) AS accident_count
@@ -71,13 +110,12 @@ FROM
     JOIN
     MSTRENGES.STATE s ON s.state_id = l.state_id
 WHERE 
-    EXTRACT(YEAR FROM a.accident_date) = rc.year AND 
-    rc.state_id = 'FL' AND
-    rc.percent_acceptable_miles between 80 AND 89
+    EXTRACT(YEAR FROM a.accident_date) = rc.year
 GROUP BY
     rc.year,
     rc.percent_acceptable_miles,
     s.name
 ORDER BY
     s.name ASC,
-    rc.year ASC;
+    rc.year ASC
+FETCH FIRST 100 ROWS ONLY;
