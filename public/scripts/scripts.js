ec5d140d9798451d3a453788035eaf7a62acf625
thomas-martin-uf <thomas.martin@ufl.edu>
2024-04-21 16:06:08 -0400

    ui overhaul, adding all queries and styling across the board, added nav bar

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,5 +1,12 @@
 import { showLoader, hideLoader } from "./utils.js";
-import { getQuery1, highlightKeywords } from "./queries.js";
+import {
+    getQuery1,
+    getQuery2,
+    getQuery3,
+    getQuery4,
+    getQuery5,
+    highlightKeywords,
+} from "./queries.js";
 
 // submits a query, takes an element
 const querySubmitHandler = async (element) => {
@@ -7,6 +14,7 @@ const querySubmitHandler = async (element) => {
     const queryNumber = element.id[element.id.length - 1];
     const ul = document.getElementById(`response-${queryNumber}`);
     const timeDiv = document.getElementById(`time-${queryNumber}`);
+    console.log("queryNumber: ", queryNumber);
 
     // clear previous time and results
     timeDiv.innerText = "Results: ";
@@ -68,7 +76,29 @@ const queryManager = {
         if (!this.queries[queryNumber]) {
             this.queries[queryNumber] = {};
         }
-        this.queries[queryNumber].queryString = getQuery1(...params);
+
+        let queryString = "";
+        switch (queryNumber) {
+            case 1:
+                queryString = getQuery1(...params);
+                break;
+            case 2:
+                queryString = getQuery2(...params);
+                break;
+            case 3:
+                queryString = getQuery3(...params);
+                break;
+            case 4:
+                queryString = getQuery4(...params);
+                break;
+            case 5:
+                queryString = getQuery5(...params);
+                break;
+            default:
+                break;
+        }
+
+        this.queries[queryNumber].queryString = queryString;
         this.queries[queryNumber].queryHTML = highlightKeywords(
             this.queries[queryNumber].queryString
         );
@@ -91,6 +121,15 @@ const queryManager = {
 
         document.getElementById("queryInput-2").innerHTML =
             queryManager.getQueryHTML(2);
+
+        document.getElementById("queryInput-3").innerHTML =
+            queryManager.getQueryHTML(3);
+
+        document.getElementById("queryInput-4").innerHTML =
+            queryManager.getQueryHTML(4);
+
+        document.getElementById("queryInput-5").innerHTML =
+            queryManager.getQueryHTML(5);
     },
 };
 
@@ -105,10 +144,14 @@ document.addEventListener("DOMContentLoaded", () => {
     var minstances = M.Modal.init(elems);
     var elems = document.querySelectorAll(".collapsible");
     var cinstances = M.Collapsible.init(elems);
+    //var elems = document.querySelectorAll(".collapsible");
+    //M.UpdateTextFields();
 
-    queryManager.setQuery(1, "FL");
-    queryManager.setQuery(2);
-    queryManager.setQuery(3);
+    queryManager.setQuery(1, "FL", "Orlando");
+    queryManager.setQuery(2, "FL");
+    queryManager.setQuery(3, "FL", 4.3);
+    queryManager.setQuery(4, "FL");
+    queryManager.setQuery(5, "FL", "GA");
     queryManager.renderQueries();
 
     // add event listeners to query form submit event for each form we have
@@ -137,10 +180,71 @@ document.addEventListener("DOMContentLoaded", () => {
         querySubmitHandler(e.target);
     });
 
+    // event listeners for query 1
     document.getElementById("states-1").addEventListener("change", (e) => {
-        queryManager.setQuery(1, e.target.value);
-        document.getElementById("queryInput-1").innerHTML =
-            queryManager.getQueryHTML(1);
+        const city = document.getElementById("states-1").value;
+        if (city) {
+            queryManager.setQuery(1, e.target.value, city);
+            document.getElementById("queryInput-1").innerHTML =
+                queryManager.getQueryHTML(1);
+        }
+    });
+
+    document.getElementById("city-1").addEventListener("change", (e) => {
+        const state = document.getElementById("states-1").value;
+        if (state) {
+            queryManager.setQuery(1, state, e.target.value);
+            document.getElementById("queryInput-1").innerHTML =
+                queryManager.getQueryHTML(1);
+        }
+    });
+
+    // event listeners for query 2
+    document.getElementById("states-2").addEventListener("change", (e) => {
+        queryManager.setQuery(2, e.target.value);
+        document.getElementById("queryInput-2").innerHTML =
+            queryManager.getQueryHTML(2);
+    });
+
+    // event listeners for query 3
+    document.getElementById("states-3").addEventListener("change", (e) => {
+        queryManager.setQuery(3, e.target.value);
+        document.getElementById("queryInput-3").innerHTML =
+            queryManager.getQueryHTML(3);
+    });
+
+    document.getElementById("rate-3").addEventListener("change", (e) => {
+        const state = document.getElementById("states-3").value;
+        queryManager.setQuery(3, state, e.target.value);
+        document.getElementById("queryInput-3").innerHTML =
+            queryManager.getQueryHTML(3);
+    });
+
+    // event listeners for query 4
+    document.getElementById("states-4").addEventListener("change", (e) => {
+        queryManager.setQuery(4, e.target.value);
+        document.getElementById("queryInput-4").innerHTML =
+            queryManager.getQueryHTML(4);
+    });
+
+    document.getElementById("states-5").addEventListener("change", (e) => {
+        const state2 = document.getElementById("states-5-2").value;
+
+        if (state2) {
+            queryManager.setQuery(5, e.target.value, state2);
+            document.getElementById("queryInput-5").innerHTML =
+                queryManager.getQueryHTML(5);
+        }
+    });
+
+    document.getElementById("states-5-2").addEventListener("change", (e) => {
+        const state1 = document.getElementById("states-5").value;
+
+        if (state1) {
+            queryManager.setQuery(5, state1, e.target.value);
+            document.getElementById("queryInput-5").innerHTML =
+                queryManager.getQueryHTML(5);
+        }
     });
 
     // start the user on the input form so they can type right away
